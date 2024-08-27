e56dde68637603a339e5d7939dc7439c48c924e4
Sumir Vats <sumirvats@gmail.com>
2024-08-27 17:13:27 +0530

    fixed model selection drowpdown

--- app/page.js
+++ app/page.js
@@ -1,60 +1,35 @@
 "use client";
-
 import React, { useState } from "react";
 import Image from "next/image";
 import Prompt from "@/components/Prompt";
 import Cards from "@/components/Cards";
-import DropdownComponent from "@/components/Dropdown";
 
 const Home = () => {
   const [selectedModel, setSelectedModel] = useState("Groq - Llama 70b");
   const [isChatActive, setIsChatActive] = useState(false);
 
-  const data = [
-    { label: "Groq - Llama 70b", value: "groq" },
-    { label: "GPT-4o", value: "gpt-4o" },
-    { label: "ChatGPT-4", value: "gpt-4" },
-    { label: "GPT-4o mini", value: "gpt-4o-mini" },
-    { label: "GPT-3.5 Turbo", value: "gpt-3.5-turbo" },
-    { label: "DALL·E", value: "dalle" },
-    { label: "Gemini 1.5 Pro", value: "gemini-1.5-pro" },
-    { label: "Gemini 1.5 Flash", value: "gemini-1.5-flash" },
-    { label: "Gemini 1.0 Pro", value: "gemini-1.0-pro" },
-  ];
-
-  const handleModelChange = (value) => {
-    setSelectedModel(value);
-  };
-
   return (
-    <div className="flex-grow container mx-auto px-4 flex flex-col">
+    <div className='flex-grow container mx-auto px-4 flex flex-col'>
       {!isChatActive && (
         <>
-          <div className="flex items-center justify-center mt-8">
+          <div className='flex items-center justify-center mt-8'>
             <Image
-              src="/icons/logo.svg"
-              alt="ChatSphere logo"
-              width="100"
-              height="100"
-              className="mr-6"
+              src='/icons/logo.svg'
+              alt='ChatSphere logo'
+              width='100'
+              height='100'
+              className='mr-6'
             />
-            <h1 className="text-6xl sm:text-5xl md:text-6xl font-bold text-primary">
+            <h1 className='text-6xl sm:text-5xl md:text-6xl font-bold text-primary'>
               ChatSphere
             </h1>
           </div>
-          <div className="flex-grow flex flex-col justify-center items-center mt-12">
+          <div className='flex-grow flex flex-col justify-center items-center mt-12'>
             <Cards />
           </div>
         </>
       )}
-      <div className="w-full h-full px-2 sm:px-4 md:px-8 lg:px-16">
-        <div className="hidden sm:block w-56 lg:w-64">
-          <DropdownComponent
-            data={data}
-            placeholder={selectedModel}
-            onSelect={handleModelChange}
-          />
-        </div>
+      <div className='w-full h-full px-2 sm:px-4 md:px-8 lg:px-16'>
         <Prompt
           selectedModel={selectedModel}
           chatActive={isChatActive}
--- components/Navbar.js
+++ components/Navbar.js
@@ -17,6 +17,24 @@ const Navbar = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const router = useRouter();
+  const [selectedModel, setSelectedModel] = useState("Groq - Llama 70b");
+  const [isChatActive, setIsChatActive] = useState(false);
+
+  const data = [
+    { label: "Groq - Llama 70b", value: "groq" },
+    { label: "GPT-4o", value: "gpt-4o" },
+    { label: "ChatGPT-4", value: "gpt-4" },
+    { label: "GPT-4o mini", value: "gpt-4o-mini" },
+    { label: "GPT-3.5 Turbo", value: "gpt-3.5-turbo" },
+    { label: "DALL·E", value: "dalle" },
+    { label: "Gemini 1.5 Pro", value: "gemini-1.5-pro" },
+    { label: "Gemini 1.5 Flash", value: "gemini-1.5-flash" },
+    { label: "Gemini 1.0 Pro", value: "gemini-1.0-pro" },
+  ];
+
+  const handleModelChange = (value) => {
+    setSelectedModel(value);
+  };
 
   return (
     <nav
@@ -24,24 +42,31 @@ const Navbar = () => {
         isDarkMode ? "dark" : ""
       }`}
     >
-      <div className="max-w-7xl mx-auto px-2">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center space-x-4">
+      <div className='max-w-7xl mx-auto px-2'>
+        <div className='flex items-center justify-between h-16'>
+          <div className='flex items-center space-x-4'>
             <button
-              className="text-primary dark:text-quaternary dark:hover:text-primary transition-colors p-2 rounded-full"
+              className='text-primary dark:text-quaternary dark:hover:text-primary transition-colors p-2 rounded-full'
               onClick={() => router.push("/")}
             >
               <IoMenu size={24} />
             </button>
             <Image
-              src="/icons/file.svg"
-              alt="New Chat"
+              src='/icons/file.svg'
+              alt='New Chat'
               width={24}
               height={24}
-              className="text-secondary dark:text-quaternary"
+              className='text-secondary dark:text-quaternary'
             />
+            <div className='hidden sm:block w-56 lg:w-64'>
+              <DropdownComponent
+                data={data}
+                placeholder={selectedModel}
+                onSelect={handleModelChange}
+              />
+            </div>
           </div>
-          <div className="flex items-center space-x-2 sm:space-x-4">
+          <div className='flex items-center space-x-2 sm:space-x-4'>
             <NavButton icon={<IoDocumentTextOutline size={20} />} />
             <NavButton icon={<IoShareOutline size={20} />} />
             <NavButton
@@ -55,7 +80,7 @@ const Navbar = () => {
               onClick={() => setIsDarkMode(!isDarkMode)}
             />
             <NavButton icon={<IoStarOutline size={20} />} />
-            <button className="text-primary hover:text-primary/80 transition-colors p-1 rounded-full">
+            <button className='text-primary hover:text-primary/80 transition-colors p-1 rounded-full'>
               <IoPersonCircleOutline size={32} />
             </button>
           </div>
@@ -67,7 +92,7 @@ const Navbar = () => {
 
 const NavButton = ({ icon, onClick }) => (
   <button
-    className="text-secondary hover:text-primary dark:text-quaternary dark:hover:text-primary transition-colors p-2 rounded-full"
+    className='text-secondary hover:text-primary dark:text-quaternary dark:hover:text-primary transition-colors p-2 rounded-full'
     onClick={onClick}
   >
     {icon}
