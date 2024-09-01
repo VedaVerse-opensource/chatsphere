1ff20b59454e7dbfef64e12e7376e761c492dd20
Sumir Vats <sumirvats@gmail.com>
2024-09-01 18:49:35 +0530

    Modified Card Data

--- components/Cards.js
+++ components/Cards.js
@@ -1,20 +1,21 @@
 import React from "react";
 
 const cardsData = [
-  "Complete my travel blog tagline: explore, discover, and...",
-  "Suggest innovative tech gadgets, with key features and drawbacks.",
-  "Plan engaging icebreakers for our virtual conference...",
-  "Outline a marketing strategy for launching our new app.",
+  "ChatSphere is an open source project built by the students of VedaVerse",
+  "Chats are stored right in your browser, providing complete privacy",
+  "Save money by using a single subscription for many AI Models with ChatSphere",
+  "Visit GitHub and start Contributing!",
 ];
 
 const Cards = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-12 w-[70%]">
+    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-12 w-[70%]'>
       {cardsData.map((text, index) => (
         <div
           key={index}
-          className="bg-quaternary dark:bg-secondary rounded-2xl w-[90%] border border-red-200 p-6 hover:shadow-lg transition-shadow duration-300">
-          <p className="text-secondary text-sm dark:text-quaternary">{text}</p>
+          className='bg-quaternary dark:bg-secondary rounded-2xl w-[90%] border border-red-200 p-6 hover:shadow-lg transition-shadow duration-300 text-center cursor-pointer'
+        >
+          <p className='text-secondary text-sm dark:text-quaternary'>{text}</p>
         </div>
       ))}
     </div>
