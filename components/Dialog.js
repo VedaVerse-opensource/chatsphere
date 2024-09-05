6fc0cf390bc687f6991cceae212e55cd7e4be056
Sumir Vats <sumirvats@gmail.com>
2024-09-05 09:01:12 +0530

    Fix dialog height

--- components/Dialog.js
+++ components/Dialog.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Dialog = ({ onClose, children }) => (
   <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4'>
-    <div className='bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden w-full max-w-4xl mx-auto h-[80vh]'>
+    <div className='bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden w-full max-w-4xl mx-auto h-[70vh]'>
       <div className='p-4 h-full flex flex-col'>
         <button
           className='text-gray-400 hover:text-gray-600 dark:hover:text-gray-300 self-end text-2xl sm:text-3xl'
